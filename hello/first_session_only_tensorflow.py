8f9d7611c4ca8f722e03b30a7d5e1bba365e49e2
mesmerli <mesmerli@hotmail.com>
2017-10-24 17:35:05 +0800

    hello folder

---
+++ hello/first_session_only_tensorflow.py
@@ -0,0 +1,14 @@
+#first_session_only_tensorflow.py
+
+import tensorflow as tf
+
+x = tf.constant(1, name='x')
+y = tf. Variable(x+9, name='y')
+
+#model = tf.initialize_all_variables()
+model = tf.global_variables_initializer()
+
+with tf.Session() as session:
+    session.run(model)
+    print(session.run(y))
+
